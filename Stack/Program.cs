ff62c8a28d88788201f85370c9f1914fc06210d7
DESKTOP-9FQ8PPR\Emma Junes <emmaastrom91@gmail.com>
2022-11-23 12:45:17 +0100

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Stack
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+           var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+            Console.ReadLine();
+
+         
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿namespace Stack
+{
+    internal partial class Program
+    {
+        public class Stack
+        {
+            public List<object> list = new List<object>();
+
+            public void Push(object obj)
+            {
+                if (obj == null)
+                {
+                    throw new InvalidOperationException("Input can not be null!");
+                }
+                else
+                {
+                    list.Add(obj);
+                }
+                    
+            }
+
+       
+            public object Pop()
+            {
+                if(list.Count == 0)
+                {
+                    throw new InvalidOperationException("List is empty!");
+                }
+
+                var index = list.Count - 1; 
+                var item = list[index]; 
+                list.RemoveAt(index);
+                return item;
+
+            }
+
+            public void Clear()
+            {
+                list.Clear();
+
+               
+            }
+
+        }
+    }
+}
